76613a94625eba848d55c114ef6011333667a1dc
Javier-rgb <javier.rgb@hotmail.com>
2021-11-01 19:51:00 -0700

    Data filter

---
+++ data_filter.py
@@ -0,0 +1,99 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+
+def run():
+    # Lists Comprehensions
+    py_devs = [worker['name'] for worker in DATA if worker['language'] == 'python']
+    platzi_workers = [worker['name'] for worker in DATA 
+                        if worker['organization'] == 'Platzi']
+
+    # High Order Functions
+    adults = list(filter(lambda worker: worker['age'] >= 18, DATA))              
+    adults = list(map(lambda worker: worker['name'], adults))
+
+    # code in next line does not work because of the python version installed.
+    # elderly = list(map(lambda worker: worker | {'elderly': worker['age'] > 69}, DATA))
+
+    for worker in py_devs:
+        print(worker)
+
+    for worker in platzi_workers:
+        print(worker)
+
+    for worker in adults:
+        print(worker)
+
+
+if __name__ == '__main__':
+    run()
