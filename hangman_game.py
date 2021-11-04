db27a591ff9b82785a44a02868b4475d22531768
Javier-rgb <javier.rgb@hotmail.com>
2021-11-03 22:58:59 -0700

    hangman game basic form, done

--- hangman_game.py
+++ hangman_game.py
@@ -1,5 +1,6 @@
 import random
 import os
+import sys
 
 def get_word(num):
     with open("./files/DATA.txt", "r") as sw:
@@ -14,31 +15,32 @@ def display_screen(word):
         empty.append("_")
     return empty
 
-def check_letter(x, array, empty):
-    for i, value in enumerate(array):
-        if value == x:
-            empty[i] = x
-
 
 def run():
-    num = random.randint(0, 170)   
+    energy = 5   
 
-    secret_word = get_word(num)
-
+    secret_word = get_word(random.randint(0, 170))
+    secret_word = secret_word.upper()
     secret_array = list(secret_word)
     secret_array.pop(len(secret_array) - 1) # Removing '\n'
 
     empty = display_screen(secret_array)
-
+    
     while empty != secret_array:
-        print("Welcome to Hangman Game")
-        print(secret_array)
+        print("Welcome to Hangman Game [type EXIT to quit] \tEnergy: " + str(energy))
+        # print(secret_array)
         print(empty)
 
-        letter = input("type a letter: ")
-        check_letter(letter, secret_array, empty)
-        os.system("clear")
+        letter = input("type a letter: ").upper()
+        if letter == "EXIT":
+            sys.exit()
+        
+        # check_letter(letter, secret_array, empty)
+        for i, value in enumerate(secret_array):
+            if value == letter:
+                empty[i] = letter
 
+        os.system("clear")
 
 if __name__ == '__main__':
     run()
