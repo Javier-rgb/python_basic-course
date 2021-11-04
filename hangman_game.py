bc62e4f2d8ae74b549fc80ea46bed02cb41f8a2e
Javier-rgb <javier.rgb@hotmail.com>
2021-11-03 20:44:28 -0700

    got position

--- hangman_game.py
+++ hangman_game.py
@@ -6,18 +6,46 @@ def get_word(num):
             if i == num:
                 return value
 
+
+def display_screen(word):
+    empty = []
+    for i in word:
+        empty.append("_")
+    return empty
+
+def get_position(x, array):
+    for i, value in enumerate(array):
+        if value == x:
+            print(i)
+
+
+
+def write():
+    pass
+
 def run():
     num = random.randint(0, 170)
     print("Welcome to Hangman Game")
 
-    secret_word = get_word(num)
+    #secret_word = get_word(num)
+
+    secret_word = "Manzana "
     new_array = list(secret_word)
 
     new_array.pop(len(new_array) - 1)
 
+    empty = display_screen(new_array)
+
+    #new_dix = {letter: new_array.index(letter) for letter in new_array}
+
+    get_position('a', new_array)
+
     print(secret_word)
     print(new_array)
     print(len(new_array))
+    # print(new_dix)
+    print(empty)
+
 
 
 if __name__ == '__main__':
